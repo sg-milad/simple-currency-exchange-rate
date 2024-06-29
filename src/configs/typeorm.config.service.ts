29767a3627aefa0e7cd131837db11be06436b261
milad <miladbachari14@gmail.com>
2024-06-29 19:16:40 +0330

    feat: add typeorm config

---
+++ src/configs/typeorm.config.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+    private dbConfig;
+
+    constructor(protected readonly configService: ConfigService) {
+        this.dbConfig = configService.get("database.main");
+    }
+
+    public createTypeOrmOptions(): TypeOrmModuleOptions {
+        return {
+            type: "postgres",
+            host: this.dbConfig.host,
+            port: this.dbConfig.port,
+            username: this.dbConfig.username,
+            password: this.dbConfig.password,
+            database: this.dbConfig.database,
+            synchronize: true,
+            logging: false,
+            // migrationsRun: true,
+            // migrations: ["dist/migrations/*migration.js"],
+            // entities: ["dist/**/entities/*.entity.{js,ts}"],
+        };
+    }
+}
