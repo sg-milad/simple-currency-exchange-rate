1fd0c181d6d3b09bbf17153bbec23c03792ff3d3
milad <miladbachari14@gmail.com>
2024-06-29 19:17:14 +0330

    feat: add modules to amm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModuleOptions } from './configs/config-module-options';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './configs/typeorm.config.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(ConfigModuleOptions()),
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useClass: TypeOrmConfigService,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
