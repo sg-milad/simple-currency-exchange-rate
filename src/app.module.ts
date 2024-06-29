e4ab513c1ecce1c999c9013002e8c7ecb83381b4
milad <miladbachari14@gmail.com>
2024-06-29 21:04:48 +0330

    feat: add exchange rate module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,17 @@ import { ConfigModuleOptions } from './configs/config-module-options';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './configs/typeorm.config.service';
+import { ExchangeRateModule } from './exchangeRate/exchange.rate.Module';
 
 @Module({
-  imports: [ConfigModule.forRoot(ConfigModuleOptions()),
-  TypeOrmModule.forRootAsync({
-    imports: [ConfigModule],
-    inject: [ConfigService],
-    useClass: TypeOrmConfigService,
-  })],
+  imports: [
+    ConfigModule.forRoot(ConfigModuleOptions()),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useClass: TypeOrmConfigService,
+    }),
+    ExchangeRateModule],
   controllers: [AppController],
   providers: [AppService],
 })
