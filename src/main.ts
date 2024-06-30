12058608aef9ccdee8553c6f8f467121222f0669
milad <miladbachari14@gmail.com>
2024-06-30 21:51:41 +0330

    feat: add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import swagger from "./shared/swagger";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     swagger.configure(app);
+    app.useGlobalPipes(new ValidationPipe({ always: true, whitelist: true }));
     await app.listen(3000);
 }
 bootstrap();
