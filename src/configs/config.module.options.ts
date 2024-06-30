3fbffe6dcc4f1e2ddb4bc11709444f5ed7033d35
milad <miladbachari14@gmail.com>
2024-06-30 10:15:46 +0330

    refactor: rename config module

--- src/configs/config.module.options.ts
+++ src/configs/config.module.options.ts
@@ -1,12 +1,13 @@
 import { productionEnv } from "src/shared/helper/helper";
 import databaseConfig from "./database.config";
 import { ConfigModuleOptions } from "@nestjs/config";
+import apiKeyConfig from "./api.key.config";
 
 export function ConfigModuleOptions(): ConfigModuleOptions {
     const options: ConfigModuleOptions = {};
     options.isGlobal = true;
     options.cache = true;
-    options.load = [databaseConfig];
+    options.load = [databaseConfig, apiKeyConfig];
     if (!productionEnv()) {
         options.envFilePath = `.env.${process.env.NODE_ENV}`;
     }
