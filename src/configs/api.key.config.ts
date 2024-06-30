9babc8d4da8d05642bc427c3447723cfcb61923a
milad <miladbachari14@gmail.com>
2024-06-30 21:52:02 +0330

    feat: add api key config

--- src/configs/api.key.config.ts
+++ src/configs/api.key.config.ts
@@ -1,5 +1,7 @@
 import { registerAs } from "@nestjs/config";
 
 export default registerAs("apiKey", () => ({
-    navasan: process.env.NAVASAN_API_KEY
+    navasan: process.env.NAVASAN_API_KEY,
+    beacon: process.env.BEACON,
+    compare: process.env.COMPARE
 }));
