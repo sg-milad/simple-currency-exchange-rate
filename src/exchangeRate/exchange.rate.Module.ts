1854c649ffade5f619ad3946e3dfe493746b381b
milad <miladbachari14@gmail.com>
2024-06-30 21:52:29 +0330

    feat: add controller to module

--- src/exchangeRate/exchange.rate.Module.ts
+++ src/exchangeRate/exchange.rate.Module.ts
@@ -4,9 +4,11 @@ import { ExchangeRateService } from "./exchange.rate.service";
 import { RequestService } from "./../shared/request/services/request.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ExchangeRateEntity } from "./entity/exchange.rate.entity";
+import { ExchangeRateController } from "./exchange.controller";
 
 @Module({
     imports: [HttpModule, TypeOrmModule.forFeature([ExchangeRateEntity])],
     providers: [ExchangeRateService, RequestService],
+    controllers: [ExchangeRateController]
 })
-export class ExchangeRateModule {}
+export class ExchangeRateModule { }
