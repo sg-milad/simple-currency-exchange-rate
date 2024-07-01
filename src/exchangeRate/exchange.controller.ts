a3ca3cf4c8475c66ebe80c850dd1f05653961449
milad <miladbachari14@gmail.com>
2024-07-01 11:13:03 +0330

    refactor: exchange controller

--- src/exchangeRate/exchange.controller.ts
+++ src/exchangeRate/exchange.controller.ts
@@ -1,16 +1,30 @@
-import { Controller, Get, Query } from "@nestjs/common";
+import { Controller, Get, Query, UseInterceptors } from "@nestjs/common";
 import { ExchangeRateService } from "./exchange.rate.service";
-import { ExchangeRateDto } from "./dto/exchange.rate.dto";
+import { ExchangeRateIrrDto } from "./dto/exchange.rate.irr.dto";
+import { ExchangeRateFiatDto } from "./dto/exchange.rate.fiat.dto";
+import { ExchangeRateInterceptor } from "src/shared/Interceptors/exchange.rate.interceptor";
 
-@Controller()
+@Controller("/exchange/convert")
+@UseInterceptors(ExchangeRateInterceptor)
 export class ExchangeRateController {
     constructor(
         private exchangeService: ExchangeRateService
     ) { }
 
-    @Get('convert')
-    async convert(@Query() data: ExchangeRateDto): Promise<number> {
+    @Get('fiat/irr')
+    async convertIrr(@Query() data: ExchangeRateIrrDto) {
         const { amount, fromCurrency, toCurrency } = data
-        return await this.exchangeService.convert(fromCurrency, toCurrency, amount);
+        return await this.exchangeService.convertIrr(fromCurrency, toCurrency, amount);
+    }
+    @Get('fiat')
+    async convertFiat(@Query() data: ExchangeRateFiatDto) {
+        const { amount, fromCurrency, toCurrency } = data
+        return await this.exchangeService.convertFiat(fromCurrency, toCurrency, amount);
+    }
+
+    @Get('crypto')
+    async convertCryptoCurrency(@Query() data: ExchangeRateFiatDto) {
+        const { amount, fromCurrency, toCurrency } = data
+        return await this.exchangeService.convertCryptoCurrency(fromCurrency, toCurrency, amount);
     }
 }
