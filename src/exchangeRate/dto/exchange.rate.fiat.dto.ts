e9700d66b25ca6b58079efca71edaf5f1c42a97c
milad <miladbachari14@gmail.com>
2024-07-01 11:12:32 +0330

    feat: add more validation

--- src/exchangeRate/dto/exchange.rate.fiat.dto.ts
+++ src/exchangeRate/dto/exchange.rate.fiat.dto.ts
@@ -1,18 +1,24 @@
 import { ApiProperty } from "@nestjs/swagger"
 import { Transform, Type } from "class-transformer"
-import { IsIn, IsNotEmpty, IsNumber, IsString } from "class-validator"
+import { IsNotEmpty, IsNumber, IsString, Matches } from "class-validator"
 
 export class ExchangeRateFiatDto {
     @ApiProperty({ type: String })
     @IsNotEmpty()
     @IsString()
     @Transform(({ value }) => value.toUpperCase())
+    @Matches(/^[A-Z]{3}$/, {
+        message: 'toCurrency must be a valid currency code (3 uppercase letters)'
+    })
     fromCurrency: string
 
     @ApiProperty({ type: String })
     @IsNotEmpty()
     @IsString()
     @Transform(({ value }) => value.toUpperCase())
+    @Matches(/^[A-Z]{3}$/, {
+        message: 'toCurrency must be a valid currency code (3 uppercase letters)'
+    })
     toCurrency: string
 
     @ApiProperty({ type: Number })
--- src/exchangeRate/dto/exchange.rate.irr.dto.ts
+++ src/exchangeRate/dto/exchange.rate.irr.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger"
 import { Transform, Type } from "class-transformer"
-import { IsIn, IsNotEmpty, IsNumber, IsString } from "class-validator"
+import { IsIn, IsNotEmpty, IsNumber, IsString, Matches } from "class-validator"
 
 export class ExchangeRateIrrDto {
     @ApiProperty({ type: String })
@@ -8,6 +8,9 @@ export class ExchangeRateIrrDto {
     @IsString()
     @Transform(({ value }) => value.toUpperCase())
     @IsIn(["USD", "IRR"])
+    @Matches(/^[A-Z]{3}$/, {
+        message: 'toCurrency must be a valid currency code (3 uppercase letters)'
+    })
     fromCurrency: string
 
     @ApiProperty({ type: String })
@@ -15,6 +18,9 @@ export class ExchangeRateIrrDto {
     @IsString()
     @Transform(({ value }) => value.toUpperCase())
     @IsIn(["USD", "IRR"])
+    @Matches(/^[A-Z]{3}$/, {
+        message: 'toCurrency must be a valid currency code (3 uppercase letters)'
+    })
     toCurrency: string
 
     @ApiProperty({ type: Number })
