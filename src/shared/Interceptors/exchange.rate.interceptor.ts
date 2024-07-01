fb4377fb2ddc3826dd3601aaa5430547095e7c95
milad <miladbachari14@gmail.com>
2024-07-01 11:14:48 +0330

    feat: add interceptor

---
+++ src/shared/Interceptors/exchange.rate.interceptor.ts
@@ -0,0 +1,19 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ExchangeRateInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        const request = context.switchToHttp().getRequest();
+        const { fromCurrency, toCurrency, amount } = request.query;
+
+        if (fromCurrency === toCurrency) {
+            return of({ amount: Number(amount) });
+
+        }
+        return next.handle().pipe(
+            map(data => data)
+        );
+    }
+}
